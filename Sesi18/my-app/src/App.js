fa3c5bab0c2b6273253435ffca86d510fc8c36d4
Esra <esradelimamanurung1999@gmail.com>
2022-01-05 15:21:30 +0700

    Sesi 18

---
+++ Sesi18/my-app/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+
+
+
+function App() { // == stateless component sebelum React 16.8
+  return ( // == render() dari class component
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        {/* Tempatkan component Header seperti contoh ini */}
+        <Header/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
